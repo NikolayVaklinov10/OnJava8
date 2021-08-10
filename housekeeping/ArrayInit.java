b723410394d44752f9f62b2c31bfaf16d0a8be02
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-08-10 23:33:57 +0100

    the autoboxing example

---
+++ housekeeping/ArrayInit.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class ArrayInit {
+  public static void main(String[] args) {
+    Integer[] a = {
+      1, 2,
+      3, // Autoboxing
+    };
+    Integer[] b = new Integer[]{
+      1, 2,
+      3, // Autoboxing
+    };
+    System.out.println(Arrays.toString(a));
+    System.out.println(Arrays.toString(b));
+  }
+}
+/* Output:
+[1, 2, 3]
+[1, 2, 3]
+*/
