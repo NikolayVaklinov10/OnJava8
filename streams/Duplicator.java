14bc975b26a7db6c6b7b90604ac3d84c962b2ddd
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-08-16 23:25:05 +0100

    duplicating code with java stream

---
+++ streams/Duplicator.java
@@ -0,0 +1,15 @@
+
+import java.util.stream.*;
+
+public class Duplicator {
+  public static void main(String[] args) {
+    Stream.generate(() -> "duplicate")
+      .limit(3)
+      .forEach(System.out::println);
+  }
+}
+/* Output:
+duplicate
+duplicate
+duplicate
+*/
