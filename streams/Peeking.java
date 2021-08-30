c4563cb354e7519be1300aa7584da5ef9f3a1c3f
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-08-30 23:05:29 +0100

    peek in java streams

---
+++ streams/Peeking.java
@@ -0,0 +1,19 @@
+
+
+class Peeking  {
+  public static void
+  main(String[] args) throws Exception {
+    FileToWords.stream("Cheese.dat")
+      .skip(21)
+      .limit(4)
+      .map(w -> w + " ")
+      .peek(System.out::print)
+      .map(String::toUpperCase)
+      .peek(System.out::print)
+      .map(String::toLowerCase)
+      .forEach(System.out::print);
+  }
+}
+/* Output:
+Well WELL well it IT it s S s so SO so
+*/
