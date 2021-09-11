58bfe1e9cb136e09c77460f322846831d9fb3d2e
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-09-11 23:45:56 +0100

    array list adding element

---
+++ arrays/ArrayCopying.java
@@ -0,0 +1,35 @@
+// arrays/ArrayCopying.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+// Demonstrate Arrays.copy() and Arrays.copyOf()
+import java.*;
+import java.util.ArrayList;
+import java.util.ArrayList.*;
+import onjava.*;
+
+public class ArrayCopying {
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> testArray = new ArrayList<Integer>();
+    testArray.add(5);
+    testArray.add(2);
+    testArray.add(4);
+    testArray.add(6);
+    testArray.add(1);
+    testArray.add(3);
+
+    System.out.println(testArray);
+
+
+
+
+
+
+
+
+
+  }
+}
+
