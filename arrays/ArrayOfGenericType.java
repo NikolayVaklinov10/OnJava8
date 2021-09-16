7bdd43be408822008cfb5b260f02a070b1f4c26c
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-09-16 22:57:06 +0100

    array with generics

---
+++ arrays/ArrayOfGenericType.java
@@ -0,0 +1,13 @@
+
+
+public class ArrayOfGenericType<T> {
+  T[] array; // OK
+  @SuppressWarnings("unchecked")
+  public ArrayOfGenericType(int size) {
+    // error: generic array creation:
+    //- array = new T[size];
+    array = (T[])new Object[size]; // unchecked cast
+  }
+  // error: generic array creation:
+  //- public <U> U[] makeArray() { return new U[10]; }
+}
