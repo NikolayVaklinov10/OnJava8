4b734791adb1bee37d3f284a39ed6e468cabeedb
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-10-15 22:23:03 +0100

    a 2D array example

---
+++ arrays/AssemblingMultidimensionalArrays.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class AssemblingMultidimensionalArrays {
+  public static void main(String[] args) {
+    Integer[][] a;
+    a = new Integer[3][];
+    for(int i = 0; i < a.length; i++) {
+      a[i] = new Integer[3];
+      for(int j = 0; j < a[i].length; j++)
+        a[i][j] = i * j; // Autoboxinga 2D
+    }
+    System.out.println(Arrays.deepToString(a));
+  }
+}
+/* Output:
+[[0, 0, 0], [0, 1, 2], [0, 2, 4]]
+*/
